7eb8f3ba8c75e3f915b81d3244094f65153df376
sergpolly <sergpolly@gmail.com>
2019-03-30 23:52:19 -0400

    log-hist and easier group extraction

--- hic_scaling_plot.m
+++ hic_scaling_plot.m
@@ -11,12 +11,22 @@ sep = table.Var3 - table.Var2;
 % aiming to do :
 hist(sep);
 
+% log-spaced histogram matlab ...
+y=100*rand(1000,1); % create random data
+x=logspace(-1,2,20); % create bin edges with logarithmic scale
+histogram(y,x); % create the plot
+set(gca,'xscale','log'); % scale the x-axis
+
+
 % also demonstrate 
 scatter(table.Var3,table.Var2,0.5,'red')
 
 
-% figure out grouping better:
-
+% figure out grouping better - something like that:
+sep1 = table.Var3(G==2) - table.Var2(G==2);
+sep1 = table.Var3(G==1) - table.Var2(G==1);
+sep2 = table.Var3(G==2) - table.Var2(G==2);
+sep3 = table.Var3(G==3) - table.Var2(G==3);
 
 strands=table(:,4:5);
 % strand2=table{5};
