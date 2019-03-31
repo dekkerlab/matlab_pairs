afd1f177aa09042308030202a8952554457a86ea
sergpolly <sergpolly@gmail.com>
2019-03-30 23:43:36 -0400

    just trying scatter, separations, etc

--- hic_scaling_plot.m
+++ hic_scaling_plot.m
@@ -1,6 +1,23 @@
 filename = 'U54_HFF_plate_subset.txt';
 table = readtable(filename);
 head(table)
+
+
+% way to etract columns from table:
+% table.Var3 - 3rd column
+
+% genomic separation here:
+sep = table.Var3 - table.Var2;
+% aiming to do :
+hist(sep);
+
+% also demonstrate 
+scatter(table.Var3,table.Var2,0.5,'red')
+
+
+% figure out grouping better:
+
+
 strands=table(:,4:5);
 % strand2=table{5};
 G=findgroups(strands);
