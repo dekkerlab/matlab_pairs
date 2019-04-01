9e74b472cd59a312ad33df8ef71095216862b850
Sergey Venev <sergpolly@gmail.com>
2019-04-01 11:53:22 -0400

    added description, sounds like a plan
    @betulakgol approves !

--- hic_scaling_plot.m
+++ hic_scaling_plot.m
@@ -1,3 +1,5 @@
+
+%  1.
 % read some pair data in the form of a table
 filename = 'U54_HFF_plate_subset.txt';
 table = readtable(filename);
@@ -6,6 +8,36 @@ table.Properties.VariableNames = {'chrom','pos1','pos2','str1','str2'};
 table.Properties.VariableUnits = {'string', 'int32', 'int32', 'char', 'char'};
 % check to see how it looks like
 head(table)
+
+% what is a chrom length
+% from the data ?
+% chromlen = max(max(pos1),max(pos2)) - best you could do from this data 
+
+%  2.
+% explain what the data is:
+% columns in the table - what they are?
+% strands - understand them better !
+
+%  3.
+% after we show the table with pais
+% let's go straight ahead to the scatter
+% of pos2 pos1, which would demonstrate
+% distance decay, i.e. more interactions
+% between nearby loci (small genomic separation)
+
+% how does this even look like ... 
+scatter(table.pos2,table.pos1,0.5,'red')
+% also it would ultimately lead to the question of
+% interaction frequency - which pairs themselves do not
+% represent - it's their "density" ...
+
+%  4.
+% let's just look at the overall distribution
+% of interactions with genomic separation
+% see how much it depends on the type of bins (unfortunately)
+% but it's fine
+% distance decay is still there - just depends on how you look
+%  at it
 
 % calculate genomic separation for the pairs
 % using nice named columns of the table:
@@ -22,15 +54,57 @@ loglog(xout, n)
 % set(gca,'YScale','log');
 % set(gca,'XScale','log');
 
-
 %  how about log-spaced bins ...
 bins=logspace(1,7,20); % create bin edges with logarithmic scale
 [n, xout] = hist(table.sep,bins);
 plot(xout, n);
 % loglog(xout, n);
 
-% how does this even look like ... 
-scatter(table.pos1,table.pos2,0.5,'red')
+%  5.
+% what else is in the data ?
+% we could group pairs by strands - and
+% immediately interesting thing pops up:
+groupsummary(table,{'str1','str2'})
+% one group is overrepresented
+% we could group table by strands and plot
+% 4 different scatters ... - should not be much difference ...
+
+G=findgroups(table,{'str1','str2'});
+G=findgroups(table.str1,table.str2});
+% do something like that to extract groups ...
+table(G==1)
+table.sep(G==1)
+% i think G -> 1,2,3,4 shoudl correspond to whatever was in
+% groupsummary - but we need to check that.
+
+%  you learn about groupsummary from G as well ...
+
+
+% what about interaction distribution with genomic separation
+% can we narrow down the differences between 4 groups to a certain
+% separation-scale ?
+
+% group, extract groups and do 4 "scalings"-counts-distributions ...
+% there must be a difference in short-range interactions
+% what does that mean ? - refer to the paper - and known HiC artifacts
+% also mention that we do not know the enzyme cut sites, yet short pairs
+% vs long pairs can be distinguished ...
+
+% discussion , understanding all of that
+
+% go to binning - decide if it's homework or not
+% anyways introduce neccessary tools/functions
+% show it briefly ...
+
+% define bins ..., e.g., bins=0:binsize:chromlen
+% use "discretize" with pre-set bins on pos1 pos2 ...
+% there is bins functions - sounds intriguing what does it do?
+% they woudl turn into 'bin1' 'bin2'
+% then initialize empty/zero matrix of num_bins*num_bins
+% loop though pairs and increment mat(bin1,bin2) += 1
+
+% unedited - to be reused:
+
 
 % let's start grouping ...
 % strands=table(:,4:5);
@@ -38,6 +112,8 @@ scatter(table.pos1,table.pos2,0.5,'red')
 % G=findgroups(strands);
 % len=length(G)
 % % k=splitapply(table,k)
+
+
 
 
 
